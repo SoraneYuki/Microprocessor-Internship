26e5496f2ef4ab74cbc708ac60baf3a83cdc7e7d
Sorane Yuki <39622241+SoraneYuki@users.noreply.github.com>
2022-12-12 23:15:32 +0800

    v20221212

--- Final Project/Project1/Project1.ino
+++ Final Project/Project1/Project1.ino
@@ -1,4 +1,20 @@
+#include <LiquidCrystal.h>
+#include <Stepper.h>
+
+#define a 2
+#define b 3
+#define a_bar 4
+#define b_bar 5
+
+LiquidCrystal lcd(49, 48, 47, 43, 42, 41);
+Stepper stepper(200, a, a_bar, b, b_bar);
+
 void setup() {
+
+    lcd.begin(0, 0);
+    lcd.clear();
+
+    stepper.setSpeed(20);
 
 }
 
